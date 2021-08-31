05f43256337fac9826f94e66c1a87a1cddfa661c
Andrewschaer <andrewschaer@github.com>
2021-08-31 13:14:53 -0400

    connected index.js, api/server.js and api/posts/poster-router.js servers and routers

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
+const express = require('express');
+
 // require your posts router and connect it here
+
+const server = express();
+
+server.use(express.json());
+
+
+// ENDPOINTS HERE
+
+
+module.exports = server;
+
