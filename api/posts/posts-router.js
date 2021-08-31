ebda8563d83cee578e76595180cdd739c0a797e3
Andrewschaer <andrewschaer@github.com>
2021-08-31 14:08:26 -0400

    first two endpoints (get all posts & get post by id) added w/ tests passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,5 +3,30 @@ const express = require('express');
 const Post = require('./posts-model');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Post.find()
+        .then(allPosts => {
+            res.status(200).json(allPosts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({message: "The posts information could not be retrieved"});
+        });
+});
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({message: "The post with the specified ID does not exist"});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({message: "The post information could not be retrieved"})
+        });
+});
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 // implement your server here
 const express = require('express');
+const postsRouter = require('./posts/posts-router')
 
 // require your posts router and connect it here
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/posts', postsRouter);
 
 
 // ENDPOINTS HERE
